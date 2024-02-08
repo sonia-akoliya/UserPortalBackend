fb74de1ec6f1a7921fdfa1ca54582a1e8ad7d679
soniaakoliya <yuktaakoliya@gmail.com>
2024-02-08 18:08:12 +0530

    chnges

---
+++ UserPortal/UserPortal/Data/UserPortalContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UserPortal.Model;
+
+namespace UserPortal.Data
+{
+    public class UserPortalContext : DbContext
+    {
+        public UserPortalContext (DbContextOptions<UserPortalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserPortal.Model.User> User { get; set; } = default!;
+    }
+}
---
+++ UserPortal/UserPortal/Model/User.cs
@@ -0,0 +1,12 @@
+﻿namespace UserPortal.Model
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
